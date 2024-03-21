c4b0847a16cccb04760e0afcb6c0a802902e21a9
Everton jr. Abreu <everton@jrabreu.com>
2024-03-20 22:38:16 -0300

    prisma

---
+++ prisma/seeds.ts
@@ -0,0 +1,42 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    username: 'alice'
+  },
+  {
+    name: 'Nilu',
+    email: 'nilu@prisma.io',
+    username: 'nilu'
+  },
+  {
+    name: 'Mahmoud',
+    email: 'mahmoud@prisma.io',
+    usermane: 'mahmoud'
+  },
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
